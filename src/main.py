b5014ca865f9bdc04ed317e7ed46b13077cbd9ca
Samuele Stasi <samuele.stasi2002@gmail.com>
2021-12-09 14:59:59 +0100

    Finishhhhhhh bitchhh

--- src/main.py
+++ src/main.py
@@ -2,15 +2,18 @@ from bot import Bot
 from threading import Thread
 from saveData import BotData
 
+bot = Bot()
+
 def makeList():
-  bot = Bot()
   bot.addFollowedToList()
 
 
 def sendMessages():
   message='placeholder'
-  bot = Bot()
   bot.sendMessages(delay=1, message=message)
   
 makeListThread = Thread(target = makeList)
 makeListThread.start()
+
+# sendDmThread = Thread(target = sendMessages)
+# sendDmThread.start()
